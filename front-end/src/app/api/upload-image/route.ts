8280674657b3eec98c6130f41ecee063c167dbb6
firasboudhraa <fboudhraa2@gmail.com>
2024-08-04 12:58:27 +0100

    uploading image done

--- front-end/src/app/api/upload-image/route.ts
+++ front-end/src/app/api/upload-image/route.ts
@@ -1,38 +1,21 @@
-import { NextResponse } from 'next/server';
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-import { IncomingMessage } from 'http';
+import { writeFile } from 'fs/promises'
+import { NextRequest, NextResponse } from 'next/server'
 
-// Setup multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadDir = path.join(process.cwd(), 'public/gallery');
-    console.log('Upload directory:', uploadDir);
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    const fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
-    console.log('Uploading file:', fileName);
-    cb(null, fileName);
-  },
-});
+export async function POST(request: NextRequest) {
+  const data = await request.formData()
+  const file: File | null = data.get('file') as unknown as File
 
-const upload = multer({ storage: storage });
+  if (!file) {
+    return NextResponse.json({ success: false })
+  }
 
-export async function POST(req: Request) {
-  return new Promise((resolve, reject) => {
-    upload.single('file')(req as unknown as IncomingMessage, {} as any, (err: any) => {
-      if (err) {
-        console.error('Error during file upload:', err);
-        reject(NextResponse.json({ error: 'File upload failed' }, { status: 500 }));
-      } else {
-        console.log('File uploaded successfully');
-        resolve(NextResponse.json({ message: 'File uploaded successfully' }, { status: 200 }));
-      }
-    });
-  });
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+
+
+  const path = `public/gallery/${file.name}`
+  await writeFile(path, buffer)
+  console.log(`open ${path} to see the uploaded file`)
+
+  return NextResponse.json({ success: true })
 }
